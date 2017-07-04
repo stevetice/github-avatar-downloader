ddbea6c9a3e1314a5d28d2956881191bf0fcdce5
stevetice <tice.steve@gmail.com>
2017-07-03 18:12:23 -0700

    add requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,30 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+var GITHUB_USER = "YOUR USERNAME HERE";
+var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
